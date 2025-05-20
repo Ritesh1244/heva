9093bb610e26574b5e90099c7236fab4c17a8897
Ritesh1244 <rite2379@gmail.com>
2025-05-20 19:49:55 +0530

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-
 import './App.css'
 import Navbar from './componenets/Navbar'
 import Body from './componenets/body'
-import Footer from './componenets/Footer'
+
 
 
 function App() {
-
-
   return (
     <>
     <div className='bg-[#0E0C14]'>
